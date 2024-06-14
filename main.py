87f76d2e2aa60626691db1942b84445dd7139a64
0xClonaz <136977848+0xClonaz@users.noreply.github.com>
2024-06-14 16:38:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,24 @@ def check_password():
         return jsonify({'found_in_leaked_databases': str(pw2), 'leaked': True})
     else:
         return jsonify({'leaked': False})
+        
+def search_email():
+    data = request.json
+    if not data or 'terms' not in data or 'types' not in data:
+        return jsonify({'error': 'Invalid input data'}), 400
+
+    url = "https://api-experimental.snusbase.com/data/search"
+    headers = {
+        "Auth": "sbx39mh542d0oshydtx3oes9whn1ay",
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to fetch data from database'}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
